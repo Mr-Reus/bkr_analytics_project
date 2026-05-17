188982642596f800458a27b94ce879a2c17570ee
Mr-Reus <130160619+Mr-Reus@users.noreply.github.com>
2026-05-18 00:01:51 +0300

    feat: додано клієнтську валідацію часу життя JWT

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,49 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, Button, TextField, Typography, Paper, Alert, CssBaseline } from '@mui/material';
 import  api  from '../api/axios';
 
+const isTokenValid = (token: string | null): boolean => {
+    if (!token) return false;
+    try {
+        const parts = token.split('.');
+        if (parts.length !== 3) return false;
+
+        const base64Url = parts[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(
+            atob(base64)
+                .split('')
+                .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+                .join('')
+        );
+
+        const payload = JSON.parse(jsonPayload);
+        const currentTime = Math.floor(Date.now() / 1000);
+        
+        return payload.exp && payload.exp > currentTime; 
+    } catch (error) {
+        return false; 
+    }
+};
+
+
 export const Login: React.FC = () => {
     const navigate = useNavigate();
     const [isRegister, setIsRegister] = useState(false);
-    
+    React.useEffect(() => {
+        const token = localStorage.getItem('access_token');
+        
+        if (token) {
+            if (isTokenValid(token)) {
+                navigate('/dashboard');
+            } else {
+                localStorage.removeItem('access_token');
+            }
+        }
+    }, [navigate]);
     const [companyName, setCompanyName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
